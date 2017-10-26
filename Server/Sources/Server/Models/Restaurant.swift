d81e7b9719367540b6c7fb5d61c62e76e89ad2e5
Matt Dias <matt.dias6@gmail.com>
2017-10-25 22:59:50 -0400

    added routes for creating and viewing restaurants TODO: add delete (for just in case)

--- Server/Sources/Server/Models/Restaurant.swift
+++ Server/Sources/Server/Models/Restaurant.swift
@@ -11,12 +11,21 @@ import PostgresStORM
 class Restaurant: PostgresStORM {
     var id: Int = 0
     var name: String = ""
+    var phoneNumber: String = ""
+    var latitude: Float = 0.0
+    var longitude: Float = 0.0
+    var website: String = ""
+
 
     override open func table() -> String { return "restaurants" }
 
     override func to(_ this: StORMRow) {
         id = this.data["id"] as? Int ?? 0
         name = this.data["name"] as? String ?? ""
+        phoneNumber = this.data["phonenumber"] as? String ?? ""
+        latitude = this.data["latitude"] as? Float ?? 0.0
+        longitude = this.data["longitude"] as? Float ?? 0.0
+        website = this.data["website"] as? String ?? ""
     }
 
     func rows() -> [Restaurant] {
@@ -32,7 +41,11 @@ class Restaurant: PostgresStORM {
     func asDictionary() -> [String: Any] {
         return [
             "id": self.id,
-            "short": self.name,
+            "name": self.name,
+            "phoneNumber": self.phoneNumber,
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            "website": self.website,
         ]
     }
 }
--- Server/Sources/Server/main.swift
+++ Server/Sources/Server/main.swift
@@ -11,11 +11,20 @@ PostgresConnector.password = "perfect"
 PostgresConnector.database = "swiftfest"
 PostgresConnector.port = 5432
 
+let restaurant = Restaurant()
+try? restaurant.setup()
+
 let server = HTTPServer()
 server.serverPort = 8080
 
 var routes = Routes()
 
+
+/// Returns a list of all restaurants in the database
+///
+/// - Parameters:
+///   - request: HTTP Request made by the consumer
+///   - response: HTTP Response made of the Restaurants in the Database
 func getRestaurants(request: HTTPRequest, response: HTTPResponse) {
     do {
         let objectQuery = Restaurant()
@@ -37,7 +46,42 @@ func getRestaurants(request: HTTPRequest, response: HTTPResponse) {
     }
 }
 
+
+/// Creates a Reservation object in the database if all parameters are present
+///
+/// - Parameters:
+///   - request: HTTP Request made by the consumer
+///   - response: HTTP Response made of the Restaurants in the Database
 func createRestaurants(request: HTTPRequest, response: HTTPResponse) {
+    do {
+        guard let json = try request.postParams[0].0.jsonDecode() as? [String: Any],
+              let name = json["name"] as? String,
+              let phoneNumber = json["phoneNumber"] as? String,
+              let latitude = json["latitude"] as? Float,
+              let longitude = json["longitude"] as? Float,
+              let website = json["website"] as? String else {
+
+            response.setBody(string: "Missing Parameter")
+                    .completed(status: .badRequest)
+            return
+        }
+
+        let restaurant = Restaurant()
+        restaurant.name = name
+        restaurant.phoneNumber = phoneNumber
+        restaurant.latitude = latitude
+        restaurant.longitude = longitude
+        restaurant.website = website
+
+        try restaurant.save { id in
+            restaurant.id = id as! Int
+        }
+
+        response.completed(status: .created)
+
+    } catch let error {
+        print(error)
+    }
 }
 
 func createReservation(request: HTTPRequest, response: HTTPResponse) {
