be9a853e5c1303ec12caa1d4af0e0725a278b899
Matt Dias <matt.dias6@gmail.com>
2018-04-14 17:13:49 -0400

    updated server project to the latest things

--- Server/Package.swift
+++ Server/Package.swift
@@ -1,11 +1,20 @@
-// Generated automatically by Perfect Assistant Application
-// Date: 2017-10-25 00:22:38 +0000
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
+
 let package = Package(
-	name: "SwiftFestServer",
-	targets: [],
-	dependencies: [
-		.Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", Version(3,0,0)),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", Version(3,0,1)),
-	]
+    name: "SwiftFestServer",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.4"),
+        .package(url: "https://github.com/SwiftORM/Postgres-StORM.git", from: "3.1.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Server",
+            dependencies: ["PerfectHTTPServer","PostgresStORM"]),
+    ]
 )
