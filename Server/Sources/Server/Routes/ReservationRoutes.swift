ffca9cdcbf236bf640561d2a9ab2592f1fb840cc
Matt Dias <matt.dias6@gmail.com>
2018-06-09 16:40:51 -0400

    in the interest of time, I'm removing the GET endpoints for Reservation. They will still be left available in the master branch

--- Server/Sources/Server/Routes/ReservationRoutes.swift
+++ Server/Sources/Server/Routes/ReservationRoutes.swift
@@ -4,58 +4,6 @@ import StORM
 import PostgresStORM
 
 struct ReservationRoutes {
-
-    
-
-    
-
-    static func getReservation(request: HTTPRequest, response: HTTPResponse) {
-        guard let id = Int(request.urlVariables["id"] ?? "0"), id > 0 else {
-            response.completed(status: .badRequest)
-            return
-        }
-
-        let reservation = Reservation()
-
-        do {
-            try reservation.get(id)
-
-            if reservation.id != 0 {
-                try response.setBody(json: reservation.asDictionary())
-                            .completed(status: .ok)
-            } else {
-                response.setBody(string: "Reservation Not Found")
-                        .completed(status: .notFound)
-                return
-            }
-        } catch let error {
-            print(error)
-        }
-
-        response.completed(status: .internalServerError)
-    }
-
-    static func getReservations(request: HTTPRequest, response: HTTPResponse) {
-        do {
-            let objectQuery = Reservation()
-            try objectQuery.findAll()
-            var responseJson: [[String: Any]] = []
-
-            for row in objectQuery.rows() {
-                responseJson.append(row.asDictionary())
-            }
-
-            try response.setBody(json: responseJson)
-                        .completed(status: .ok)
-        } catch let error as StORMError {
-            response.setBody(string: error.string())
-                    .completed(status: .internalServerError)
-        } catch let error {
-            response.setBody(string: "\(error)")
-                    .completed(status: .internalServerError)
-        }
-    }
-
     
 
     static func updateReservation(request: HTTPRequest, response: HTTPResponse) {
